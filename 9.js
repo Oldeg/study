fcab62598cbc66f3b9085b24201ac6ccc720371a
Oldeg <9helgi9@gmail.com>
2022-02-23 15:51:48 +0200

    done

---
+++ 9.js
@@ -0,0 +1,35 @@
+const sum = 10 + 10;
+
+const difference = 45 - 33;
+
+const product = 8 * 10;
+
+const quotient = 66 / 33;
+
+let myVar = 87;
+// Only change code below this line
+myVar ++;
+
+let myVar = 11;
+// Only change code below this line
+myVar --;
+
+const ourDecimal = 5.7;
+// Only change code below this line
+const myDecimal = 5.7
+
+const product = 2.0 * 2.5;
+
+const quotient = 4.4 / 2.0; 
+
+const remainder = 11 % 3;
+
+let a = 3;
+let b = 17;
+let c = 12;
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+console. log(a,b,c);
+
