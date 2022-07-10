4144aecf9e640a7e90ad71cd267323d157df4d1e
Oldeg <9helgi9@gmail.com>
2022-07-10 09:12:37 +0200

    done

---
+++ code15.js
@@ -0,0 +1,21 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {"artist": "David Hit",
+      "title": "Eowmdom Rodkow",
+      "release_year": 1974,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ]}
+  ];
