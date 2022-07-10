fe6859fefa1fca1572a76c1ba7662950bb8f4cfc
Oldeg <9helgi9@gmail.com>
2022-07-10 08:44:25 +0200

    done

---
+++ code9.js
@@ -0,0 +1,9 @@
+
+const testObj = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water"
+  };
+  
+  let entreeValue = testObj ["an entree"];
+  let drinkValue = testObj ["the drink"];
