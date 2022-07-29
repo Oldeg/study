3f80a2c12c80eaa281d9d0471373de625e7f5cb0
Oldeg <9helgi9@gmail.com>
2022-07-29 14:47:17 +0200

    done

---
+++ 72.js
@@ -0,0 +1,5 @@
+function htmlColorNames(arr) {
+    arr.splice(0,2,'DarkSalmon', 'BlanchedAlmond')
+    return arr;
+  }
+  console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
