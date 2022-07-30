f0880d09510a296b6f45daf94ab3a3b9000af1c3
Oldeg <9helgi9@gmail.com>
2022-07-30 18:30:10 +0200

    done

---
+++ 75.js
@@ -0,0 +1,8 @@
+let myNestedArray = [
+ 
+    ['unshift', false, 1, 2, 3, 'complex', 'nested'],
+    [['loop', 'shift', 6, 7, 1000, 'method','deep']],
+    [[['concat', false, true, 'spread', 'array','deeper']]],
+    [[[['mutate', 1327.98, 'splice', 'slice', 'push', 'deepest']]]],
+    
+  ];
