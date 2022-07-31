898be30642597e167f684476b9f5b8aebab87b2e
Oldeg <9helgi9@gmail.com>
2022-07-31 18:53:39 +0200

    done

---
+++ 82.js
@@ -0,0 +1,12 @@
+function findLongestWordLength(str) {
+    const strSplit = str.split(' ');
+    let longestWord = 0;
+    for( let i=0;i<strSplit.length;i++ ){
+      if (strSplit[i].length > longestWord){
+        longestWord = strSplit[i].length
+      }
+    }
+    return longestWord;
+  }
+  
+  findLongestWordLength("The quick brown fox jumped over the lazy dog");
