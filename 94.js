12f63ab9d1da9a6d587736ab2c462ddea19840e6
Oldeg <9helgi9@gmail.com>
2022-08-06 09:16:03 +0200

    done

--- 94.js
+++ 94.js
@@ -3,11 +3,11 @@ function Dog(name) {
   }
   Dog.prototype = {
     constructor: Dog,
-    numLegs: 4,
+    numLegs: 4, 
     eat: function() {
       console.log("nom nom nom");
     },
     describe: function() {
       console.log("My name is " + this.name);
     }
-  };  
+  }; 
