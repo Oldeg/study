72c41fec8ec3815847fc39d8854360009fd98d8f
Oldeg <9helgi9@gmail.com>
2022-08-07 09:30:58 +0200

    done

---
+++ code106.js
@@ -0,0 +1,17 @@
+function getGrade (s1, s2, s3) {
+    let res = (s1+s2+s3)/3;
+    if (res>=90){
+      return 'A';
+    } 
+    if (res>=80){
+      return 'B';
+    } 
+    if (res>=70){
+      return 'C'
+    } 
+    if (res>=60){
+      return 'D';
+    } else {
+      return 'F';
+    }
+  }
