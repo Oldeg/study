d7c87ea2fe015e2cc3c955e882a2f751ea25e466
Oldeg <9helgi9@gmail.com>
2022-08-08 08:46:50 +0200

    done

--- 96.js
+++ 96.js
@@ -5,4 +5,4 @@ function Dog(name) {
   let beagle = new Dog("Snoopy");
   
   Dog.prototype.isPrototypeOf(beagle);  
-  Object.prototype.isPrototypeOf(Dog.prototype);    
+  Object.prototype.isPrototypeOf(Dog.prototype);
