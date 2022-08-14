3915bea6c5898742e0ad57363f526a0d8b5926c7
Oldeg <9helgi9@gmail.com>
2022-08-14 09:55:13 +0200

    done

--- code113.js
+++ code113.js
@@ -1,4 +1,7 @@
 function rpsls(pl1,pl2){
+    if (pl1 == pl2){
+      return 'Draw!'
+    }
     if (pl1 == 'rock'&&pl2 == 'lizard'||
         pl1 == 'rock'&&pl2 == 'scissors'||
         pl1 == 'paper'&&pl2 == 'rock'||
@@ -10,17 +13,5 @@ function rpsls(pl1,pl2){
         pl1 == 'lizard'&&pl2 == 'paper'||
         pl1 == 'lizard'&&pl2 == 'spock')
         {return 'Player 1 Won!'}
-    if (pl1 == 'rock'&&pl2 == 'paper'||
-        pl1 == 'rock'&&pl2 == 'spock'||
-        pl1 == 'paper'&&pl2 == 'scissors'||
-        pl1 == 'paper'&&pl2 == 'lizard'||
-        pl1 == 'spock'&&pl2 == 'lizard'||
-        pl1 == 'spock'&&pl2 == 'paper'||
-        pl1 == 'scissors'&&pl2 == 'spock'||
-        pl1 == 'scissors'&&pl2 == 'rock'||
-        pl1 == 'lizard'&&pl2 == 'rock'||
-        pl1 == 'lizard'&&pl2 == 'scissors'){
-        return 'Player 2 Won!'
-        } 
-    return 'Draw!';
-}
+       return 'Player 2 Won!'
+    }
