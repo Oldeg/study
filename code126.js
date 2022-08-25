30c53175e253f1e816728ade93f212f91b92d7f1
Oldeg <9helgi9@gmail.com>
2022-08-25 16:08:26 +0200

    done

---
+++ code126.js
@@ -0,0 +1,7 @@
+function findLowestInt(k, n = 1) {
+    while (n <= Number.MAX_SAFE_INTEGER) {
+      let k1 = n * k, k2 = n * (k + 1), s1 = k1.toString(), s2 = k2.toString()
+      if (s1.split('').sort().join('') === s2.split('').sort().join('')) return n
+      n++
+    }
+  }
