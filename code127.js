a7eab06f1a6fc6125ee5c3ceb6a0f6463c5c17ce
Oldeg <9helgi9@gmail.com>
2022-08-26 20:14:35 +0200

    done

--- code127.js
+++ code127.js
@@ -3,4 +3,4 @@ function splitTheBill(x) {
     let arr=Object.keys(x);
     let arr2=Object.values(x).map((a,b)=>(a-average));
     return Object.assign(...arr.map((n, i) => ({ [n]: parseFloat((arr2[i]).toFixed(2)) })));
-    } 
+    }  
