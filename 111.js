d0fb36713c39ade90d54bb0e65d0059c914324f4
Oldeg <9helgi9@gmail.com>
2022-09-03 11:24:56 +0200

    done

---
+++ 111.js
@@ -0,0 +1,7 @@
+function spinalCase(str) {
+    let spinal = str.replace(/(?!^)([A-Z])/g, ' $1')
+                 .replace(/[_\s]+(?=[a-zA-Z])/g, '-').toLowerCase();
+   return spinal 
+ }
+ 
+ spinalCase('This Is Spinal Tap');
