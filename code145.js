b97c5d35d65fe076270bfe4013ed8c2bf955dcc8
Oldeg <9helgi9@gmail.com>
2022-09-07 11:19:41 +0200

    done

---
+++ code145.js
@@ -0,0 +1,12 @@
+function findNb(m) {
+    let i=1;
+    let count=0;
+    while (m>0){
+        m-=Math.pow(i,3)
+        i++
+        count++
+        }
+        
+        
+        return (m === 0) ? count : -1;
+    }
